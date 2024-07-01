c494e75d1bfebc85dee72c78f6a544fa751cb7b1
Emmy Manning <emswoo@EM.local>
2024-07-01 12:44:59 -0400

    connected to repo router

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const repoRouter = require('./routes/repo');
+
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
-})
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', repoRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
-  });
+  console.log(`Server started on port ${process.env.PORT}`);
+});
