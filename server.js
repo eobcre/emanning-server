55e92732d9fc8147f8a97438ebbd30b1835ea4a5
Emmy Manning <emswoo@EM.local>
2024-07-05 14:51:53 -0400

    email router

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const repoRouter = require('./routes/repo');
+const emailRouter = require('./routes/email');
 
 dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', repoRouter);
+app.use('/api', emailRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
